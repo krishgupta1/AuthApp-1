979b9072d03cfa115facace18ae0f24667df71a5
krishgupta1 <krishgupta11110@gmail.com>
2023-07-21 21:25:09 +0530

    first commit 21-

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+require('dotenv').config();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+require("./config/database").connect();
+
+// Route import and mount
+const user = require("./route/user");
+app.use("/api/v1", user);
+
+// 
+
+app.listen(PORT, () => {
+    console.log(`APP is listening at ${PORT}`)
+})
